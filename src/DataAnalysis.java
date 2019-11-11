10fd5cac28451a162864c98afb3e4e7210ee5bf3
amannischal <57366963+amannischal@users.noreply.github.com>
2019-11-10 21:37:42 -0500

    updated
    added helper method, removing the unnecessary data cleaner class

--- src/DataAnalysis.java
+++ src/DataAnalysis.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class DataAnalysis {
-	private DataCleaner dc = new DataCleaner(); 
 	private ArrayList<Job> jobs = new ArrayList<Job>();
 	private ArrayList<String> jobTitles = new ArrayList<String>();
 	private ArrayList<String> jobLocations = new ArrayList<String>();
@@ -16,12 +15,16 @@ public class DataAnalysis {
 			
 			while (s.hasNextLine()) {
 				String line = s.nextLine();
+				// split job information based on delimter
 				String[] lineData = line.split("\\|");
-				lineData[1] = dc.cleanLocation(lineData[1]);
+				// clean the location using the helper method
+				lineData[1] = cleanLocation(lineData[1]);
+				//create a new job and it to the jobs array list
 				Job job = new Job (lineData[0],lineData[1],lineData[2],lineData[3],lineData[4]);
 		
 				jobs.add(job); 
 				
+				// create arrays of all the unique job titles, location, and companies
 				if (!jobTitles.contains(lineData[0])) {jobTitles.add(lineData[0]);}
 				if (!jobLocations.contains(lineData[1])) {jobLocations.add(lineData[1]);}
 				if (!jobCompanies.contains(lineData[2])) { jobCompanies.add(lineData[2]);}
@@ -76,6 +79,12 @@ public class DataAnalysis {
 		return keywordCounter; 
 	}
 	
+	//Helper method to clean location information
+	private String cleanLocation (String text) {
+		int n = text.indexOf(",");
+		String cleanString = text.substring(0, n+4);
+		return cleanString; 
+	}
 	
 	public static void main(String[] args) {
 		DataAnalysis da = new DataAnalysis();
