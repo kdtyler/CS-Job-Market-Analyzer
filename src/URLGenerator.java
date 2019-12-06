4725c0fbf83d54bce8253ef2de27e85acfcce03e
Bo Jiang <jiangbo.xx@gmail.com>
2019-12-05 23:07:11 -0700

    add fix to estimated salary; add checks for invalid input

--- src/URLGenerator.java
+++ src/URLGenerator.java
@@ -16,7 +16,7 @@ public class URLGenerator {
 	private String experienceLevel;
 	private String sortBy;
 	private String URL;
-	
+
 	public URLGenerator(String jobTitle, String estimatedSalary, String city, String state, String distance, 
 			String jobType, String experienceLevel, String sortBy) {
 		this.jobTitle = jobTitle;
@@ -29,54 +29,90 @@ public class URLGenerator {
 		this.sortBy = sortBy;
 		this.setURL(this.generateURL());
 	}
-	
+
 	public void setURL(String url) {
 		this.URL = url;
 	}
-	
+
 	public String getURL() {
 		return this.URL;
 	}
-	
+
 	public String generateURL() {
 		String url = "https://www.indeed.com/jobs?";
 		String[] jts = this.jobTitle.trim().split("\\s+");
 		url += ("q=" + String.join("+", jts));
 		if(!this.estimatedSalary.equals("None")) {
-			url += ("%24" + this.estimatedSalary.substring(0, 3) + "%2C" + this.estimatedSalary.substring(3, 6));
+			try {
+				int estimatedSalaryInt = Integer.parseInt(this.estimatedSalary);
+				String thousands = Integer.toString(Math.min(estimatedSalaryInt / 1000, 999));
+				String ones = Integer.toString(estimatedSalaryInt % 1000);
+				if(ones.length() == 1) {
+					ones = "00" + ones;
+				}
+				else if(ones.length() == 2) {
+					ones = "0" + ones;
+				}
+				url += ("%24" + thousands + "%2C" + ones);
+			}
+			catch(java.lang.NumberFormatException e) {
+				System.out.println("Invalid input for estimated salary !");
+			}
+
 		}
-		
+
 		String[] cities = this.city.trim().split("\\s+");
 		url += ("&l=" + String.join("+", cities) + "%2C+" + this.state.trim());
-		
+
 		if(!this.distance.equals("None")) {
-			url += ("&radius=" + this.distance);
+			try {
+				Integer.parseInt(this.distance);
+				url += ("&radius=" + this.distance);
+			}
+			catch(java.lang.NumberFormatException e) {
+				System.out.println("Invalid input for distance !");
+			}
 		}
-		
-		if(this.jobType.equals("full-time") || this.jobType.equals("internship") || jobType.equals("contract") ||
-				this.jobType.equals("part-time") || this.jobType.equals("temporary") || this.jobType.equals("commission")) {
-			url += ("&jt=" + this.jobType.replace("-", ""));
+
+		if(!this.jobTitle.equals("None")){
+			if(!(this.jobType.equals("full-time") || this.jobType.equals("internship") || jobType.equals("contract") ||
+					this.jobType.equals("part-time") || this.jobType.equals("temporary") || this.jobType.equals("commission"))) {
+				System.out.println("Invalid input for job type !");
+			}else {
+				url += ("&jt=" + this.jobType.replace("-", ""));
+			}	
 		}
-		
-		if(this.experienceLevel.equals("senior level") || this.experienceLevel.equals("mid level") || 
-				this.experienceLevel.equals("entry level")) {
-			String[] exp = this.experienceLevel.trim().split("\\s+");
-			url += ("&explvl=" + String.join("_", exp));
+
+		if(!this.experienceLevel.equals("None")) {
+			if(!(this.experienceLevel.equals("senior level") || this.experienceLevel.equals("mid level") || 
+					this.experienceLevel.equals("entry level"))) {
+				System.out.println("Invalid input for experience level !");
+			}
+			else {
+				String[] exp = this.experienceLevel.trim().split("\\s+");
+				url += ("&explvl=" + String.join("_", exp));
+			}
 		}
-		
-		if(this.sortBy.equals("date")) {
-			url += "&sort=date";
+
+		if(!this.sortBy.equals("None")) {
+			if(!(this.sortBy.equals("date") || this.sortBy.equals("relevance"))) {
+				System.out.println("Invalid input for sort by !");
+			}
+			else {
+				if(this.sortBy.equals("date")) {
+					url += "&sort=date";
+				}
+			}
 		}
-		
-		// url += "&start=0";
+
 		return url;
 	}
-	
-	
+
+
 	public static void main(String[] args) {
-		URLGenerator g = new URLGenerator("machine learning engineer", "100000", "San Francisco", "CA", "None", "None", "None", "None");
+		URLGenerator g = new URLGenerator("software engineer", "90000", "Tempe", "AZ", "None", "internship", "None", "None");
 		System.out.println(g.getURL());
 	}
-	
-	
+
+
 }
